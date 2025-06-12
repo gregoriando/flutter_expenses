6a193e352b8025d3dd995f1c942dd65ee26ad817
Gregory <gregorystp@icloud.com>
2025-06-12 19:59:31 -0300

    feat 'Construcao de classes importantes para o aprendizado da aplicação, estamos componetizando o aplicativo'

---
+++ lib/components/transaction_form.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class TransactionForm extends StatelessWidget {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+  final Function(String, double) onSubmit;
+  TransactionForm(this.onSubmit, {super.key});
+
+  void _submitForm() {
+    //Faz um submit da nova transação (botão)
+    final title = titleController.text;
+    final value = double.tryParse(valueController.text) ?? 0.0;
+    if (title.isEmpty || value <= 0) {
+      return;
+    }
+    onSubmit(title, value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            TextField(
+              controller: titleController,
+              //serve para submit no teclado para nao precisar apertar nota transacao
+              onSubmitted: (_) => _submitForm(),
+              decoration: const InputDecoration(labelText: 'Titulo'),
+            ),
+            TextField(
+              controller: valueController,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onSubmitted: (_) => _submitForm(),
+              decoration: InputDecoration(labelText: 'Valor (R\$)'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    _submitForm();
+                  },
+                  child: Text(
+                    'Nova Transação',
+                    style: TextStyle(
+                      color: Colors.purple,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
