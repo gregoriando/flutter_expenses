145156dbd2ecd7cae7aacc71ceafd7fafc633c7d
Gregory <gregorystp@icloud.com>
2025-06-14 11:09:19 -0300

    feat "InserçÃo de imagem e fonts"

--- lib/main.dart
+++ lib/main.dart
@@ -54,51 +54,51 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _transactions = [
-    Transaction(
-      id: 't1',
-      title: 'Novo Tênis de Corrida',
-      value: 310.76,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Conta de Luz',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't3',
-      title: 'Conta de água',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't4',
-      title: 'Conta de internet',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't5',
-      title: 'Conta de cartão de crédito',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-    Transaction(id: 't6', title: 'Marisa', value: 211.30, date: DateTime.now()),
-    Transaction(
-      id: 't7',
-      title: 'Riachuelo',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-    Transaction(id: 't8', title: 'Renner', value: 211.30, date: DateTime.now()),
-    Transaction(id: 't9', title: 'Iptu', value: 211.30, date: DateTime.now()),
-    Transaction(
-      id: 't10',
-      title: 'Conta de gas',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
+    // Transaction(
+    //   id: 't1',
+    //   title: 'Novo Tênis de Corrida',
+    //   value: 310.76,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't2',
+    //   title: 'Conta de Luz',
+    //   value: 211.30,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't3',
+    //   title: 'Conta de água',
+    //   value: 211.30,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't4',
+    //   title: 'Conta de internet',
+    //   value: 211.30,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't5',
+    //   title: 'Conta de cartão de crédito',
+    //   value: 211.30,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(id: 't6', title: 'Marisa', value: 211.30, date: DateTime.now()),
+    // Transaction(
+    //   id: 't7',
+    //   title: 'Riachuelo',
+    //   value: 211.30,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(id: 't8', title: 'Renner', value: 211.30, date: DateTime.now()),
+    // Transaction(id: 't9', title: 'Iptu', value: 211.30, date: DateTime.now()),
+    // Transaction(
+    //   id: 't10',
+    //   title: 'Conta de gas',
+    //   value: 211.30,
+    //   date: DateTime.now(),
+    // ),
   ];
 
   void _addTransaction(String title, double value) {
